55581046d02d219ad159d0f14b03d5584d4fca90
shanjian <shanjian@codemao.com>
2021-11-24 19:31:47 +0800

    init

---
+++ src/test/java/com/janson/skywalking/demo/SkywalkingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janson.skywalking.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
